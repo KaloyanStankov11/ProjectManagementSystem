a81f2e1a25cf65c4d0f0a00fa2bc1d1a88b19619
KaloyanStankov11 <kaloqn.t@mail.bg>
2024-07-01 17:15:31 +0300

    small fixes

--- frontend/src/app/pages/manage-projects/manage-projects.component.ts
+++ frontend/src/app/pages/manage-projects/manage-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-manage-projects',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ManageProjectsComponent {
 
+  constructor(private router: Router) {
+  }
+  goToUsersPage() {
+    this.router.navigate(["/workers"])
+  }
 }
